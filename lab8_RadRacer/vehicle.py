8b7c6e15b0c6820e797d5dcb33b311abbb112c4c
Rianne123 <145406752+Rian-stack@users.noreply.github.com>
2024-10-14 09:39:40 -0700

    initialized attributes

--- lab8_RadRacer/vehicle.py
+++ lab8_RadRacer/vehicle.py
@@ -1,3 +1,33 @@
+import random
+
 class Vehicle:
-    __init__ (self, name, initial, min_speed, max_speed)
+    '''
+    Attributes:
+        _name – vehicle’s name, 
+        _initial – vehicle’s initial, 
+        _min_speed – minimum value of speed range, 
+        _max_speed – maximum value of speed range, 
+        _position – the vehicle’s current location on the track, 
+        _energy – vehicle’s power level.
+
+    '''
+    def __init__(self, name, initial, min_speed, max_speed):
+        self._name = name
+        self._initial = initial
+        self._min_speed = min_speed
+        self._max_speed = max_speed
+
+    def fast(self, dist):
+        return "(Vehicle) quickly moves (#) units!"
+    
+    def slow(self, dist):
+        return "(Vehicle) slowly moves (#) units!"
+
+    def __str__(self):
+        return name + position + energy
+
+    
+    
+    
+        
         
