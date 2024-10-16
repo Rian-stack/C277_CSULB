c086476a16f0986c39ad49da291e6b95a96fbdf3
Rianne123 <145406752+Rian-stack@users.noreply.github.com>
2024-10-15 18:12:29 -0700

    fix: Refactor Vehicle class to improve readability and maintainability

--- lab8_RadRacer/vehicle.py
+++ lab8_RadRacer/vehicle.py
@@ -2,55 +2,6 @@ import abc
 import random
 
 class Vehicle(abc.ABC):
-    '''
-    Attributes:
-        _name – vehicle’s name, 
-        _initial – vehicle’s initial, 
-        _min_speed – minimum value of speed range, 
-        _max_speed – maximum value of speed range, 
-        _position – the vehicle’s current location on the track, 
-        _energy – vehicle’s power level.
-
-    '''
-    def __init__(self, name, initial, min_speed, max_speed):
-        self._name = name
-        self._initial = initial
-        self._min_speed = min_speed
-        self._max_speed = max_speed
-
-    def fast(self, dist):
-        if self._energy >= 5:
-            movement = random.randint(self._min_speed, self._max_speed)
-            if movement < dist:
-                self._position += movement
-                self._energy -= 5
-                return f"({self._name}) quickly moves {movement} units!"
-            else:
-                self._position += dist
-                self._energy -= 5
-                return f"({self._name}) crashes into obstacle at {dist} units!"
-        else:
-            return f"({self._name}) does not have enough energy to move quickly!"
-    
-    def slow(self, dist):
-        movement = min(self._min_speed // 2, dist)
-        self._position += movement
-        return f"({self._name}) slowly moves {movement} units!"
-
-    def __str__(self):
-        return f"{self._name} - Position: {self._position}, Energy: {self._energy}"
-    
-    @abc.abstractmethod
-    def description_string(self):
-        pass
-
-    @abc.abstractmethod
-    def special_move(self, dist):
-        pass
-import abc
-import random
-
-class Vehicle(abc.ABC):
     """
     Abstract base class for all vehicles in the race.
 
@@ -88,11 +39,13 @@ class Vehicle(abc.ABC):
                 self._energy -= 5
                 return f"({self._name}) quickly moves {movement} units!"
             else:
-                self._position += dist - 1
+                self._position = dist  # Set position to the obstacle
                 self._energy -= 5
                 return f"({self._name}) crashes into obstacle at {dist} units!"
         else:
             return f"({self._name}) does not have enough energy to move quickly!"
+
+
     
     def slow(self, dist):
         """
