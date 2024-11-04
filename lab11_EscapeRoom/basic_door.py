4c498bf3a03a38944ab297e0705f3bd0c1cae2f6
Rian-stack <paparianne728@gmail.com>
2024-11-04 09:38:07 -0800

    override abstract methods for basic door

--- lab11_EscapeRoom/basic_door.py
+++ lab11_EscapeRoom/basic_door.py
@@ -2,31 +2,31 @@ import door
 import random
 
 class Basic_Door(door.Door):
-    '''
-    A basic door. You either push or pull
-    '''
-    def __init__(self):
-        self._solution = random.randint(1, 2)
-        self._input = None
-
-    def examine_door(self):
-        return "A basic door. You can either push or pull it to open."
-
-    def menu_options(self):
-        return "1. Push\n2. Pull"
-
-    def get_menu_max(self):
-        return 2
-
-    def attempt(self, option):
-        self._input = option
-        return "" # Add description of attempt here
-
-    def is_unlocked(self):
-        return self._input == self._solution
-
-    def clue(self):
-        return "Try the other way."
-
-    def success(self):
-        return "" # Add success message here
+    '''                                                                                                                                                                                      
+     A basic door. You either push or pull                                                                                                                                                    
+     '''                                                                                                                                                                                      
+    def __init__(self):                                                                                                                                                                      
+        self._solution = random.randint(1, 2)                                                                                                                                                
+        self._input = None                                                                                                                                                                   
+                                                                                                                                                                                            
+    def examine_door(self):                                                                                                                                                                  
+        return "A basic door. You can either push or pull it to open."                                                                                                                       
+                                                                                                                                                                                            
+    def menu_options(self):                                                                                                                                                                  
+        return "1. Push\n2. Pull"                                                                                                                                                            
+                                                                                                                                                                                            
+    def get_menu_max(self):                                                                                                                                                                  
+        return 2                                                                                                                                                                             
+                                                                                                                                                                                            
+    def attempt(self, option):                                                                                                                                                               
+        self._input = option                                                                                                                                                                 
+        return "" # Add description of attempt here                                                                                                                                          
+                                                                                                                                                                                            
+    def is_unlocked(self):                                                                                                                                                                   
+        return self._input == self._solution                                                                                                                                                 
+                                                                                                                                                                                            
+    def clue(self):                                                                                                                                                                          
+        return "Try the other way."                                                                                                                                                          
+                                                                                                                                                                                            
+    def success(self):                                                                                                                                                                       
+        return "" # Add success message here    
