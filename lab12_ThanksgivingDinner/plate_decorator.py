7cf890970467b8256e8a8d0f6c59c00f8118e3cd
Rian-stack <paparianne728@gmail.com>
2024-11-18 09:55:34 -0800

    feat: Add Plate_Decorator class skeleton.

---
+++ lab12_ThanksgivingDinner/plate_decorator.py
@@ -0,0 +1,10 @@
+import abc
+import plate
+
+class Plate_Decorator():
+    '''
+    PlateDecorator – extends ABC and extends from Plate
+        a. __init__(self, p) – pass in the plate p and assign it to the _plate attribute.
+        b. description, area, weight, count methods: call each on your _plate attribute
+    '''
+    
