a05d0f8a52da3ae33157b9028137c3cf73114fb5
Rianne Papa <paparianne728@gmail.com>
2024-12-04 20:08:46 -0800

    fixed indentation errors

--- lab13_TaskList2/tasklist.py
+++ lab13_TaskList2/tasklist.py
@@ -7,6 +7,7 @@ class Tasklist(task.Task):
   _n (int)– count of iterater
   
   """
+
   def __init__(self):
     """
     initialize _task and _n. then open file to read tasks and store all tasks in tasklist, _task.
@@ -19,6 +20,7 @@ class Tasklist(task.Task):
       self._task.append(task.Task(desc, date, time))
     file.close()
     self._task.sort()
+
   def add_task(self, desc, date, time):
     """
     add new task in tasklist
@@ -29,21 +31,24 @@ class Tasklist(task.Task):
     """
     self._task.append(task.Task(desc, date, time))
     self._task.sort()
+
   def get_current_task(self):
     """
     return current task
     Return: task[0](task.Task)
     """
-      self._task.sort()
-      return self._task[0]
+    self._task.sort()
+    return self._task[0]
+  
   def mark_complete(self):
     """
     mark current task completed. return deleated task
     Return: temp(task.Task)
     """
-      temp = self._task[0]
-      self._task.pop(0)
-      return temp
+    temp = self._task[0]
+    self._task.pop(0)
+    return temp
+  
   def postpone_task(self, date, time):
     """
     postpone current task with date and time that user input.
@@ -52,10 +57,11 @@ class Tasklist(task.Task):
       date (str)– due date of the task. A string in the format: MM/DD/YYYY
       time (str)– time the task is due. A string in the format: HH:MM
     """
-      desc = self._task[0].description
-      self._task.pop(0)
-      self._task.append(task.Task(desc, date, time))
-      self._task.sort()
+    desc = self._task[0].description
+    self._task.pop(0)
+    self._task.append(task.Task(desc, date, time))
+    self._task.sort()
+
   def save_file(self):
     """
     Save all task in tasklist in text file 'tasklist.txt' 
@@ -64,18 +70,21 @@ class Tasklist(task.Task):
     for new_task in self._task:
       file.write(repr(new_task) + '\n')
     file.close()
+
   def __len__(self):
     """
     override len() function. return length of task list
     Return: len(self._task) (int)
     """
     return len(self._task)
+  
   def __iter__(self):
     """
     override __iter__ function to traverse elements of tasklist. initialize _n, then return self
     """
     self._n = 0
     return self
+  
   def __next__(self):
     """
      return the next item in the sequence.
